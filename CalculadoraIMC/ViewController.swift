93d8904b3cd56862d451f4568bf721f64599fe86
Mansour <theboom.kajas@gmail.com>
2024-12-11 20:16:42 +0100

    First version

--- CalculadoraIMC/ViewController.swift
+++ CalculadoraIMC/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var height: Int = 160
+    var weight: Float = 60.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func setHeight(_ sender: UISlider) {
+        height = Int(sender.value)
+        heightLabel.text = "\(height) cm"
+    }
+    
+    @IBAction func setWeight(_ sender: UIStepper) {
+        weight = Float(sender.value)
+        weightLabel.text = "\(weight) Kg"
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+        // Calcular el IMC
+        let heightInMeters = (Float(height) / 100.0)
+        let result = weight / pow(heightInMeters, 2)
+        resultLabel.text = String(format: "%.2f", result)
+        print("El IMC es \(result)")
+    }
 }
 
